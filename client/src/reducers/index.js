8e556ec1c302fceaa0deb74691a1f126b02e7375
dmitridrozdov <dm.drozdov@gmail.com>
2021-02-01 13:01:39 +1100

    Initial implementation

---
+++ client/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import posts from './posts'
+
+export default combineReducers({ posts })
+
+// import { combineReducers } from 'redux';
+
+// import posts from './posts';
+
+// export const reducers = combineReducers({ posts });
