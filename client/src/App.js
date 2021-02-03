9fbf00f2432415edccebd19dcbba423a6547a118
dmitridrozdov <dm.drozdov@gmail.com>
2021-02-03 19:22:31 +1100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,7 @@ const App = () => {
     return (
         <Container maxWidth='lg'>
             <AppBar className = {classes.appBar} position='static' color='inherit'>
-                <Typography className = {classes.heading} variant='h2' align='center'>Memories</Typography>
-                <img className = {classes.image} src={memories} alt='memories' height='60'></img>
+                <Typography className = {classes.heading} variant='h2' align='center'>Trawin</Typography>
             </AppBar>
             <Grow in>
                 <Container>
