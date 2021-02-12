2baf3b6cd815ba6fb2c9667e58b84bc727f95dea
dmitridrozdov <dm.drozdov@gmail.com>
2021-02-13 09:54:01 +1100

    Update posts.js

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,11 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes'
 import * as api from '../api'
 
 // Action Creator
 export const getPosts = () => async(dispatch) => {
     try {
         const { data } = await api.fetchPosts()
-        dispatch({ type: 'FETCH_ALL', payload: data })
+        dispatch({ type: FETCH_ALL, payload: data })
     } catch(error) {
         console.log(error.message)
     }   
@@ -13,7 +14,7 @@ export const getPosts = () => async(dispatch) => {
 export const createPost = (post) => async(dispatch) => {
     try {
         const { data } = await api.createPost(post)
-        dispatch({ type: 'CREATE', payload: data })
+        dispatch({ type: CREATE, payload: data })
     } catch(error) {
         console.log(error.message)
     }   
@@ -22,8 +23,17 @@ export const createPost = (post) => async(dispatch) => {
 export const updatePost = (id, post) => async(dispatch) => {
     try {
         const { data } = await api.updatePost(id, post)
-        dispatch({ type: 'UPDATE', payload: data })
+        dispatch({ type: UPDATE, payload: data })
     } catch(error) {
         console.log(error.message)
     }   
+}
+
+export const deletePost = (id) => async(dispatch) => {
+    try {
+        await api.deletePost(id)
+        dispatch({ type: DELETE, payload: id })
+    } catch(error) {
+        console.log(error)
+    }
 }
