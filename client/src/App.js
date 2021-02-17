66fb26636af1cf0a6d78f2805aad7abbcccebe09
dmitridrozdov <dm.drozdov@gmail.com>
2021-02-18 07:01:54 +1100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,47 @@ import { Container, AppBar, /*Typography,*/ Grow, Grid } from '@material-ui/core
 
 import { getPosts } from './actions/posts'
 import Form from './components/Form/Form'
-import useStyles from './styles'
+import useStyles, { CssTextField }  from './styles'
 import MainTable from './components/MainTable/MainTable'
 
 const App = () => {
     const [currentId, setCurrentId] = useState(null)
+    const [currentSearch, setCurrentSearch] = useState('')
     const classes = useStyles()
     const dispatch = useDispatch()
 
     useEffect(() => {
-        dispatch(getPosts())
-    }, [dispatch])
+        dispatch(getPosts(currentSearch))
+    }, [dispatch, currentSearch])
 
     return (
         <Container maxWidth='lg'>
             <AppBar className = {classes.appBar} position='static' color='inherit'>
                 {/* <Typography className = {classes.heading} variant='h2' align='center'>Trawin</Typography> */}
             </AppBar>
+
             <Grow in>
                 <Container>
                     <Grid container justify='space-between' alignItems='stretch' spacing={4}>
+                            <Grid item xs={12} sm={8}>
+                                <CssTextField 
+                                        className={classes.textMargin}
+                                        InputProps={{
+                                            className: classes.textStyle
+                                        }}
+                                        variant="outlined"
+                                        name='search' label='Search' fullWidth 
+                                        value={currentSearch} 
+                                        onChange={(e) => setCurrentSearch(e.target.value)}
+                                        />
+                            </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+            
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' spacing={4}>
                         <Grid item xs={12} sm={8}>
                             <MainTable setCurrentId={setCurrentId}/>
                         </Grid>
