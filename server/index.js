f473f9724e1f359749eb3710e1b35c5ee34b3cdd
dmitridrozdov <dm.drozdov@gmail.com>
2021-02-20 11:57:41 +1100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import postRouts from './routes/posts.js'
+import agentRouts from './routes/agents.js'
 
 const app = express()
 dotenv.config()
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
 
 app.use(cors())
-app.use('/posts', postRouts)
+app.use('/agents', agentRouts)
 
 const PORT = process.env.PORT || 5000
 
