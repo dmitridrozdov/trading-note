c9ed9eba3115f8a8ade15308764bad345f4c302b
dmitridrozdov <dm.drozdov@gmail.com>
2021-02-22 14:11:52 +1100

    Create agents.js

---
+++ client/src/actions/agents.js
@@ -0,0 +1,40 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes'
+import * as api from '../api'
+
+// Action Creator
+export const getAgents = (searchStr) => async(dispatch) => {
+    try {
+        const { data } = await api.fetchAgents(searchStr)
+        dispatch({ type: FETCH_ALL, payload: data })
+    } catch(error) {
+        console.log(error.message)
+    }   
+}
+
+export const createAgent = (post) => async(dispatch) => {
+    try {
+        const { data } = await api.createAgent(post)
+        dispatch({ type: CREATE, payload: data })
+    } catch(error) {
+        console.log(error.message)
+    }   
+}
+
+export const updateAgent = (id, post) => async(dispatch) => {
+    try {
+        const { data } = await api.updateAgent(id, post)
+        console.log(post)
+        dispatch({ type: UPDATE, payload: data })
+    } catch(error) {
+        console.log(error.message)
+    }   
+}
+
+export const deleteAgent = (id) => async(dispatch) => {
+    try {
+        await api.deleteAgent(id)
+        dispatch({ type: DELETE, payload: id })
+    } catch(error) {
+        console.log(error)
+    }
+}
