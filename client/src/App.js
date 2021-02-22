c624cbf8d9541f34d0560808f62e985e0cbcd410
dmitridrozdov <dm.drozdov@gmail.com>
2021-02-22 14:11:58 +1100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux'
 import { Container, AppBar, /*Typography,*/ Grow, Grid } from '@material-ui/core'
 // import memories from './images/memories.png'
 
-import { getAgents, getPosts } from './actions/posts'
+import { getAgents } from './actions/agents'
 import Form from './components/Form/Form'
 import useStyles, { CssTextField }  from './styles'
 import MainTable from './components/MainTable/MainTable'
