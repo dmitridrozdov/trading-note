14cb3ca361238b4d0a31432018eff8258622f488
dmitridrozdov <dm.drozdov@gmail.com>
2021-12-26 06:05:47 +1100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
 
 app.use(cors())
-app.use('/agents', tradingNoteRouts)
+app.use('/tradingnotes', tradingNoteRouts)
 
 // app.get('/', (req,res) => { //FOR DEPLOYMENT
 //     res.send('hello to trawin')
