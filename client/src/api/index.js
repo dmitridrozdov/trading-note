855ae0ecc4532cd63f73e4813e752590a5870bed
dmitridrozdov <dm.drozdov@gmail.com>
2021-12-26 06:05:45 +1100

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const url = 'http://localhost:5000/agents'
+const url = 'http://localhost:5000/tradingnotes'
 // const url = 'https://trawin.herokuapp.com/agents'
 
-export const fetchAgents = (searchStr) => axios.get(url, { params: { search: searchStr }})
+export const fetchTradingNotes = (searchStr) => axios.get(url, { params: { search: searchStr }})
 export const createAgent = (newPost) => axios.post(url, newPost)
 export const updateAgent = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost)
 export const deleteAgent = (id) => axios.delete(`${url}/${id}`)
