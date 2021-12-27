6c292483da0d7df46e1e181163b63adab22e0293
dmitridrozdov <dm.drozdov@gmail.com>
2021-12-27 19:08:46 +1100

    Update tradingNotes.js

--- server/controllers/tradingNotes.js
+++ server/controllers/tradingNotes.js
@@ -5,7 +5,7 @@ import TradingNote from '../models/models.js'
 export const getTradingNotes = async (req, res) => {
     try {
         let search = req.query.search
-        const tradingNotes = await TradingNote.find({agent: {$regex: search}})
+        const tradingNotes = await TradingNote.find({coin: {$regex: search}})
         res.status(200).json(tradingNotes)
     } catch(error) {
         res.status(404).json({ message: error.message})
@@ -25,9 +25,9 @@ export const createTradingNote =  async (req, res) => {
 
 export const updateTradingNote = async (req, res) => {
     const { id } = req.params;
-    const { agent, address, phone } = req.body;
+    const { coin, address, phone } = req.body;
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
-    const updatedTradingNote = { agent, address, phone, _id: id };
+    const updatedTradingNote = { coin, address, phone, _id: id };
     await TradingNote.findByIdAndUpdate(id, updatedTradingNote, { new: true });
     res.json(updatedTradingNote);
 }
