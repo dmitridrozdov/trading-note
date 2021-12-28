c4882625b405c3f25fefd33998bea8ca3b735635
dmitridrozdov <dm.drozdov@gmail.com>
2021-12-28 17:00:18 +1100

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,3 @@ import { combineReducers } from 'redux'
 import coins from './tradingNotes'
 
 export default combineReducers({ coins })
-
-// import { combineReducers } from 'redux';
-
-// import posts from './posts';
-
-// export const reducers = combineReducers({ posts });
