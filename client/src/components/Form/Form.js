6eec9cd6ff9ba1019085a8c468c6e9f1090dddd0
dmitridrozdov <dm.drozdov@gmail.com>
2021-12-30 18:52:31 +1100

    Update Form.js

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -110,8 +110,8 @@ const Form = ({ currentId, setCurrentId }) => {
                     name='closeposition' label='Close Position' fullWidth value={noteData.closeposition} 
                     onChange={(e) => setnoteData({ ...noteData, closeposition: e.target.value })}/>
                     
-                <Button className={classes.buttonSubmit} variant='contained' color='primary' size='large' type='submit' fullWidth >Create Note</Button>
-                <Button className={classes.textStyle} variant='contained' color='secondary' size='small' onClick={clear} fullWidth >Clear</Button>
+                <Button className={classes.buttonSubmit} size='small' type='submit' fullWidth >Create Note</Button>
+                <Button className={classes.buttonClear} size='small' onClick={clear} fullWidth >Clear</Button>
             </form>
         </Paper>
     )
