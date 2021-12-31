beb7af30a694ac3e630c0b9dbb168f3d582bc663
dmitridrozdov <dm.drozdov@gmail.com>
2021-12-31 11:51:02 +1100

    Create TradingNotes.jsx

---
+++ client/src/components/Cards/TradingNotes.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Grid, CircularProgress } from '@material-ui/core'
+import { useSelector } from 'react-redux'
+
+import TradingNote from './Card/TradingNote'
+import useStyles from './styles'
+
+const TradingNotes = ({ setCurrentId }) => {
+  const notes = useSelector((state) => state.notes)
+  const classes = useStyles()
+
+  return (
+    !notes.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {notes.map((note) => (
+          <Grid key={note._id} item xs={12} sm={6} md={6}>
+            <TradingNote note={note} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  )
+}
+
+export default TradingNotes
