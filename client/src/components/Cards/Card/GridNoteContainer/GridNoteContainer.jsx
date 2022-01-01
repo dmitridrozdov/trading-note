fbb204c2fb0ed44f6c811999ffb854602ee68c5c
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-01 16:56:45 +1100

    Update GridNoteContainer.jsx

--- client/src/components/Cards/Card/GridNoteContainer/GridNoteContainer.jsx
+++ client/src/components/Cards/Card/GridNoteContainer/GridNoteContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import useStyles from './styles'
 import { Typography } from '@material-ui/core/'
 
-const GridNoteContainer = ({ label1, value1, label2, value2, label3, value3 }) => {
+const GridNoteContainer = ({ label1, value1, label2, value2, label3, value3, stoplossline }) => {
     const classes = useStyles()
 
     return (
@@ -16,8 +16,8 @@ const GridNoteContainer = ({ label1, value1, label2, value2, label3, value3 }) =
                 <Typography className={classes.noteInput}>{value2}</Typography>
             </div>
             <div className={classes.column3}>
-                <Typography className={classes.noteLabel}>{label3}</Typography>
-                <Typography className={classes.noteInput}>{value3}</Typography>
+                <Typography className={stoplossline === 'true' ? classes.noteStopLabel : classes.noteLabel}>{label3}</Typography>
+                <Typography className={stoplossline === 'true' ? classes.noteStopInput : classes.noteInput}>{value3}</Typography>
             </div>
         </div>
     )
