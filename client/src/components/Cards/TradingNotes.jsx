b7e3c5fec3f98bc02547684f5a06c72c5f392eb5
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-02 13:45:54 +1100

    Update TradingNotes.jsx

--- client/src/components/Cards/TradingNotes.jsx
+++ client/src/components/Cards/TradingNotes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, CircularProgress } from '@material-ui/core'
+import { Grid, CircularProgress, Divider, Chip } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 
 import TradingNote from './Card/TradingNote'
@@ -11,15 +11,41 @@ const TradingNotes = ({ setCurrentId }) => {
 
   return (
     !notes.length ? <CircularProgress /> : (
-      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-        {notes.sort(function (a, b) {
-              return b.createdAt.localeCompare(a.createdAt)
-            }).map((note) => (
-          <Grid key={note._id} item xs={12} sm={6} md={6}>
-            <TradingNote note={note} setCurrentId={setCurrentId} />
-          </Grid>
-        ))}
-      </Grid>
+      <div>
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+          {notes
+            .filter(function(note) {
+              return note.closeposition === ''
+            })
+            .sort(function (a, b) {
+                return b.createdAt.localeCompare(a.createdAt)
+              }).map((note) => (
+            <Grid key={note._id} item xs={12} sm={6} md={6}>
+              <TradingNote note={note} setCurrentId={setCurrentId} />
+            </Grid>
+          ))}
+          
+        </Grid>
+        <br/> <br/>
+        <Divider light/>
+        <br/> <br/>
+          {/* <Chip label="CHIP" />
+        </Divider> */}
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+          {notes
+            .filter(function(note) {
+              return note.closeposition !== ''
+            })
+            .sort(function (a, b) {
+                return b.createdAt.localeCompare(a.createdAt)
+              }).map((note) => (
+            <Grid key={note._id} item xs={12} sm={6} md={6}>
+              <TradingNote note={note} setCurrentId={setCurrentId} />
+            </Grid>
+          ))}
+          
+        </Grid>
+      </div>
     )
   )
 }
