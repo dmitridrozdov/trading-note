d6bc2369a6553f1fd44e5fcfa95b4162f5d319e0
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-03 11:42:56 +1100

    Create styles.js

---
+++ client/src/components/Divider/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export default makeStyles({
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+      },
+      
+      border: {
+        borderBottom: '1px solid #ababab',
+        width: '100%',
+      },
+      
+      content: {
+        padding: '0 10px 0 10px',
+        fontFamily: 'Roboto Slab',
+        fontSize: '14px',
+        color: '#ababab',
+      },
+})
