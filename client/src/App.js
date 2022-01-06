38c1b0b698fdfe34567a0c1fc20aedd6fe036640
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-06 16:10:06 +1100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { Container, Grow, Grid } from '@material-ui/core'
-
-import { getTradingNotes } from './actions/tradingNotes'
-import Form from './components/Form/Form'
-import useStyles, { NoteTextField }  from './styles'
-import TradingNotes from './components/Cards/TradingNotes'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { TradingNoteApp } from './components/TradingNoteApp/TradingNoteApp'
 
 const App = () => {
-    const [currentId, setCurrentId] = useState(null)
-    const [currentSearch, setCurrentSearch] = useState('')
-    const classes = useStyles()
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        dispatch(getTradingNotes(currentSearch))
-    }, [dispatch, currentSearch])
-
     return (
-        <Container maxWidth='lg'>
-            <Grow in>
-                <Container>
-                    <Grid container justify='space-between' alignItems='stretch' spacing={4}>
-                            <Grid item xs={12} sm={8}>
-                                <NoteTextField 
-                                        className={classes.textMargin}
-                                        InputProps={{
-                                            className: classes.textStyle
-                                        }}
-                                        name='search' label='Search' fullWidth 
-                                        value={currentSearch} 
-                                        onChange={(e) => setCurrentSearch(e.target.value)}
-                                        />
-                            </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-            
-            <Grow in>
-                <Container>
-                    <Grid container justify='space-between' spacing={4}>
-                        <Grid item xs={12} sm={8}>
-                            <TradingNotes setCurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<TradingNoteApp/>} />
+          {/* <Route path="/forcast" exact component={ForcastComponent} /> */}
+        </Routes>
+      </BrowserRouter>
     )
 }
 
