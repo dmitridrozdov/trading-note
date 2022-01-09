f6b37a0bfac769458c454aa02e7ed5d1fafb9c87
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-09 18:56:52 +1000

    Update styles.js

--- client/src/components/Forecast/styles.js
+++ client/src/components/Forecast/styles.js
@@ -1,14 +1,57 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles'
+import { TextField } from '@material-ui/core'
 
 export default makeStyles((theme) => ({
+  textStyle: {
+    fontFamily: 'Montserrat',
+    fontSize: '12px',
+  },
 
-  paper: {
-    padding: theme.spacing(2),
-    backgroundColor: 'rgba(0,0,0,0.05)'
-    // boxShadow: 'none',
+  textMargin: {
+    marginTop: 10,
+    marginRight: 40,
   },
-  table: {
-    minWidth: 650,
-    backgroundColor: 'rgba(0,0,0,0.05)'
+
+  btnForecast: {
+    marginTop: 25,
+    marginRight: 40,
+    
+    fontFamily: 'Montserrat',
+    fontSize: '10px',
+    color: '#ed2939',
+    border: '1px solid #ed2939',
+    borderRadius: '5px',
+    '&:hover': {
+      backgroundColor: 'white',
+      color: 'darkred',
+      border: '1px solid darkred',
+    },
   },
+
 }));
+
+export const NoteTextField = withStyles({
+  root: {
+    '& label': {
+      color: 'lightgrey',
+      fontFamily: 'Montserrat',
+    },
+    '&.Mui-focused': {
+      color: 'lightgrey',
+      fontFamily: 'Montserrat',
+    },
+    '& label.Mui-focused': {
+      color: 'lightgrey',
+      fontFamily: 'Montserrat',
+    },
+    '& .MuiInput-underline:before': {
+      borderBottomColor: '#f0efed', // Semi-transparent underline
+    },
+    '& .MuiInput-underline:hover:before': {
+      borderBottomColor: '#f0efed', // Solid underline on hover
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#f0efed', // Solid underline on focus
+    },
+  },
+})(TextField);
