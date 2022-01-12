63de14d7f92324d20e80cf075d838eb63cba3bad
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-13 07:04:52 +1000

    Update Divider.jsx

--- client/src/components/Divider/Divider.jsx
+++ client/src/components/Divider/Divider.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import useStyles from './styles'
 
-const Divider = () => {
+const Divider = ({ name }) => {
     const classes = useStyles()
 
     return (
         <div className={classes.container}>
             <div className={classes.border} />
                 <span className={classes.content}>
-                    Completed
+                    {name}
                 </span>
             <div className={classes.border} />
         </div>
