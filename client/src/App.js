6af3db5bf9093fa0fcd339e39aec3e412df3f377
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-14 06:31:37 +1000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { TradingNoteApp } from './components/TradingNoteApp/TradingNoteApp'
 import { Forecast } from './components/Forecast/Forecast'
+import { HistoricalMinimum } from './components/HistoricalMinimum/HistoricalMinimum'
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<TradingNoteApp/>} />
           <Route path='/forecast' element={<Forecast />} />
+          <Route path='/minimum' element={<HistoricalMinimum />} />
         </Routes>
       </BrowserRouter>
     )
