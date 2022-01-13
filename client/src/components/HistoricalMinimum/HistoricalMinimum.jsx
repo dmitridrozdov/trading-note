b4ed946b7d2fedb3869e0cb429ea231fe8f41170
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-14 07:25:41 +1000

    Update HistoricalMinimum.jsx

--- client/src/components/HistoricalMinimum/HistoricalMinimum.jsx
+++ client/src/components/HistoricalMinimum/HistoricalMinimum.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
+const api = {
+    base: 'https://api.binance.com/api/v1/exchangeInfo',
+  }
+
+  const fetchHistoricalMinimumData = () => {
+    fetch(`${api.base}`)
+    .then((res) => res.json())
+    .then((result) => console.log(result.symbols))
+  }
+
 export const HistoricalMinimum = () => {
+    fetchHistoricalMinimumData()
     return (
         <div>
             Historical Minimum for coins
