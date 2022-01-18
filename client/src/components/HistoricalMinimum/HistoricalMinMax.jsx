fa16c49ec6b246895039a5fdfaafc089f2b27377
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-18 15:43:03 +1100

    Update HistoricalMinMax.jsx

--- client/src/components/HistoricalMinimum/HistoricalMinMax.jsx
+++ client/src/components/HistoricalMinimum/HistoricalMinMax.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { FlapperSpinner } from 'react-spinners-kit' //https://bestofreactjs.com/repo/dmitrymorozoff-react-spinners-kit--react-loader-spinners-progress-bars
 
 const api = {
     base: 'https://api.binance.com/api/v1/exchangeInfo',
   }
 
-  const fetchHistoricalMinimumData = () => {
-    fetch(`${api.base}`)
-    .then((res) => res.json())
-    .then((result) => result.symbols.map((coin) => console.log(coin.symbol)))
-  }
+
 
 export const HistoricalMinMax = () => {
-    fetchHistoricalMinimumData()
+    const [coins, setCoins] = useState([])
+
+    const fetchHistoricalMinimumData = () => {
+      fetch(`${api.base}`)
+      .then((res) => res.json())
+      .then((result) => 
+        setCoins(result.symbols.filter(
+        (coin) => coin.symbol.slice(-4) === 'USDT'))
+      )
+    }
+
+    useEffect( () => { fetchHistoricalMinimumData() }, [])
+
+    console.log(coins)
     return (
+      !coins.length ? <FlapperSpinner /> : (
         <div>
-            Historical Minimum for coins
+          {coins.map((coin) => <div>{coin.symbol}</div>)}
         </div>
-    )
+    ))
 }
