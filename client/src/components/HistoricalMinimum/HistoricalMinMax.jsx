1612b757ab1fc9024418589cefae14570b1290f3
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-18 17:03:16 +1100

    Update HistoricalMinMax.jsx

--- client/src/components/HistoricalMinimum/HistoricalMinMax.jsx
+++ client/src/components/HistoricalMinimum/HistoricalMinMax.jsx
@@ -2,16 +2,15 @@ import React, { useState, useEffect } from 'react'
 import { FlapperSpinner } from 'react-spinners-kit' //https://bestofreactjs.com/repo/dmitrymorozoff-react-spinners-kit--react-loader-spinners-progress-bars
 
 const api = {
-    base: 'https://api.binance.com/api/v1/exchangeInfo',
-  }
-
-
+    exchange: 'https://api.binance.com/api/v1/exchangeInfo',
+    klines: 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&limit=1000',
+}
 
 export const HistoricalMinMax = () => {
     const [coins, setCoins] = useState([])
 
     const fetchHistoricalMinimumData = () => {
-      fetch(`${api.base}`)
+      fetch(`${api.exchange}`)
       .then((res) => res.json())
       .then((result) => 
         setCoins(result.symbols.filter(
@@ -19,13 +18,25 @@ export const HistoricalMinMax = () => {
       )
     }
 
-    useEffect( () => { fetchHistoricalMinimumData() }, [])
+    const fetchCoinData = () => {
+      fetch(`${api.klines}`)
+      .then((res) => res.json())
+      .then((result) => {
+        const closePrice = result.map((item) => {
+          return item[4]
+        })
+        const max = Math.max(...closePrice)
+        console.log(max)
+      })
+    }
 
-    console.log(coins)
+    useEffect( () => { fetchHistoricalMinimumData() }, [])
+    useEffect( () => { fetchCoinData() }, [])
+
     return (
       !coins.length ? <FlapperSpinner /> : (
         <div>
-          {coins.map((coin) => <div>{coin.symbol}</div>)}
+          {/* {coins.map((coin) => <div>{coin.symbol}</div>)} */}
         </div>
     ))
 }
