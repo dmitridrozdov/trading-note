a2f8b2834ff0beabec641491d2c1b79be9d0a026
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-22 11:45:46 +1100

    Update HistoricalMinMax.jsx

--- client/src/components/HistoricalMinimum/HistoricalMinMax.jsx
+++ client/src/components/HistoricalMinimum/HistoricalMinMax.jsx
@@ -1,42 +1,51 @@
 import React, { useState, useEffect } from 'react'
 import { FlapperSpinner } from 'react-spinners-kit' //https://bestofreactjs.com/repo/dmitrymorozoff-react-spinners-kit--react-loader-spinners-progress-bars
+import axios from 'axios'
 
 const api = {
     exchange: 'https://api.binance.com/api/v1/exchangeInfo',
-    klines: 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&limit=1000',
+    klines: 'https://api.binance.com/api/v3/klines?symbol=',
 }
 
 export const HistoricalMinMax = () => {
-    const [coins, setCoins] = useState([])
+    // const [coins, setCoins] = useState([])
+    const [coinsData, setCoinsData] = useState([])
 
     const fetchHistoricalMinimumData = () => {
-      fetch(`${api.exchange}`)
-      .then((res) => res.json())
-      .then((result) => 
-        setCoins(result.symbols.filter(
-        (coin) => coin.symbol.slice(-4) === 'USDT'))
-      )
+      axios.get(`${api.exchange}`)
+      .then((result) => {
+        const filteredCoins = result.data.symbols.filter((coin) => coin.symbol.slice(-4) === 'USDT').map((coin) => coin.symbol)
+        // setCoins(filteredCoins)
+        console.log(filteredCoins.length)
+        // const array = ['BTCUSDT', 'ETHUSDT']
+        const coinsDataPromises = filteredCoins.map((coin) => fetchCoinData(coin))
+        Promise.all(coinsDataPromises).then(results => {
+          const data = results.map(result => result[0])
+          console.log(data)
+          setCoinsData(data)
+        })
+      })
     }
 
-    const fetchCoinData = () => {
-      fetch(`${api.klines}`)
-      .then((res) => res.json())
+    async function fetchCoinData(coin) {
+      return await axios.get(`${api.klines}` + coin + '&interval=1d&limit=1000')
       .then((result) => {
-        const closePrice = result.map((item) => {
+        const closePrice = result.data.map((item) => {
           return item[4]
         })
         const max = Math.max(...closePrice)
-        console.log(max)
+        return [{coin: coin, max: max}]
       })
     }
 
     useEffect( () => { fetchHistoricalMinimumData() }, [])
-    useEffect( () => { fetchCoinData() }, [])
+
+    // console.log(coinsData)
 
     return (
-      !coins.length ? <FlapperSpinner /> : (
+      !coinsData.length ? <FlapperSpinner /> : (
         <div>
-          {/* {coins.map((coin) => <div>{coin.symbol}</div>)} */}
+          {coinsData.map((coin) => <div>{coin.coin}:{coin.max}</div>)}
         </div>
     ))
 }
