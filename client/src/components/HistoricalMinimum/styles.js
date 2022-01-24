8aed98dcd242ada06127d41c3a9803dfff7df0cf
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-24 12:05:15 +1100

    Update styles.js

--- client/src/components/HistoricalMinimum/styles.js
+++ client/src/components/HistoricalMinimum/styles.js
@@ -1,8 +1,13 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles({
-    container: {
+  styledTable: {
+    borderCollapse: 'collapse',
+    margin: '25px 0',
+    fontSize: '0.9em',
+    fontFamily: 'Varela Round',
+    minWidth: '400px',
+    boxShadow: '0 0 20px rgba(0, 0, 0, 0.15)',
+}
 
-      },
-      
 })
