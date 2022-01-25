87d1f953516ae877e487a8fb107f5705e58b39f1
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-25 16:24:47 +1100

    Update HistoricalMinMax.jsx

--- client/src/components/HistoricalMinimum/HistoricalMinMax.jsx
+++ client/src/components/HistoricalMinimum/HistoricalMinMax.jsx
@@ -19,7 +19,10 @@ export const HistoricalMinMax = () => {
         // const array = ['BTCUSDT']
         const coinsDataPromises = filteredCoins.map((coin) => fetchCoinData(coin))
         Promise.all(coinsDataPromises).then(results => {
-          const data = results.map(result => result[0])
+          const data = 
+            results
+            .map(result => result[0])
+            .sort((a, b) => parseFloat(a.ndays) - parseFloat(b.ndays))
           // console.log(data)
           setCoinsData(data)
         })
@@ -29,14 +32,15 @@ export const HistoricalMinMax = () => {
     async function fetchCoinData(coin) {
       return await axios.get(`${api.klines}` + coin + '&interval=1d&limit=1000')
       .then((result) => {
+        const ndays = result.data.length
         const closePrice = result.data.map((item) => {
           return item[4]
         })
         const max = Math.max(...closePrice)
         const current = closePrice[closePrice.length - 1]
-        const diffPercentage = ((current - max) / max) * 100
+        const diffPercentage = (((current - max) / max) * 100).toFixed(2)
         console.log('diff ' + diffPercentage)
-        return [{coin: coin, max: max, current: current, diffPercentage: diffPercentage}]
+        return [{coin: coin, max: max, current: current, diffPercentage: diffPercentage, ndays: ndays}]
       })
     }
 
@@ -45,23 +49,24 @@ export const HistoricalMinMax = () => {
     return (
       !coinsData.length ? <FlapperSpinner /> : (
         <div>
-          {/* {coinsData.map((coin) => <div>{coin.coin}   max price: {coin.max}  current price: {coin.current}   diff: {coin.diffPercentage}</div>)} */}
           <table className={classes.styledTable}>
             <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Points</th>
+                <tr className={classes.styledTableHead}>
+                    <th>Coin</th>
+                    <th>Max Price</th>
+                    <th>Current Difference</th>
+                    <th>Days on Exchange</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>Dom</td>
-                    <td>6000</td>
+              {coinsData.map((coin) => 
+                <tr className={classes.styledTableTr}>
+                  <td className={classes.styledTableCell}>{coin.coin}</td>
+                  <td className={classes.styledTableCell}>{coin.max}</td>
+                  <td className={classes.styledTableCell}>{coin.diffPercentage}</td>
+                  <td className={classes.styledTableCell}>{coin.ndays}</td>
                 </tr>
-                <tr>
-                    <td>Melissa</td>
-                    <td>5150</td>
-                </tr>
+              )}
             </tbody>
         </table>
         </div>
