5632ed9e682f22e298ebe4d13d3b7d21b4100eaf
dmitridrozdov <dm.drozdov@gmail.com>
2022-04-03 09:51:05 +1000

    Update LongTermCard.jsx

--- client/src/components/Cards/LongTermCard/LongTermCard.jsx
+++ client/src/components/Cards/LongTermCard/LongTermCard.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Card, CardContent, Button, Typography } from '@material-ui/core/'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Edit from '@material-ui/icons/Edit'
+import moment from 'moment'
+import { useDispatch } from 'react-redux'
+
+import { deleteTradingNote } from '../../../actions/tradingNotes'
+import useStyles from './styles'
 
 const LongTermCard = ({ note, setCurrentId }) => {
   return (
